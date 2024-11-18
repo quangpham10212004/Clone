01f2647bb62a8a115f5ded27623f95ad7133e769
shyboy <quangpham10212004@gmail.com>
2024-11-18 23:38:52 +0700

    day la ban mini cua wheelfinder

---
+++ src/main/java/Entity/Admin.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+import java.util.Scanner;
+
+import Interact.AddNewAccount;
+
+
+/**
+ *
+ * @author Quang
+ */
+public class Admin extends User {
+    private Operation[] operations = new Operation[] { new AddNewAccount(1) }; // 1 phan tu duy nhat
+
+
+    public Admin(){
+        super();
+    }
+    
+    public void showList(Database database , Scanner sc){
+        System.out.println("\n1. Add New Car");
+        System.out.println("2. View Car");
+        System.out.println("3. Update Car");
+        System.out.println("4. Delete Car");
+        System.out.println("5. Add New Admin");
+        System.out.println("6. Show History");
+        System.out.println("7. Quit\n");
+        
+        int i = sc.nextInt();
+        operations[0].operation(database, sc, this);
+        showList(database, sc);
+        
+    }
+}
---
+++ src/main/java/Entity/Car.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+/**
+ *
+ * @author Quang
+ */
+public class Car {
+
+    private int ID;
+    private String brand;
+    private String model;
+    private String color;
+    private int year;// nsx
+    private double price;
+    private int available;  //0 : het hang
+                            //1 : con hang
+    public Car() {
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    
+
+    
+    
+}
